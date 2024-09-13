e614f43c926475af4c807cc8695a4b6fee410071
SurrealEmil <emil.ejderklev1@hotmail.com>
2024-09-13 16:44:43 +0200

    Add project files.

---
+++ Controllers/ReservationsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BistroBookMVC.Controllers
+{
+    public class ReservationsController : Controller
+    {
+        //private readonly HttpClient _httpClient;
+
+
+        //public ReservationsController(HttpClient httpClient)
+        //{
+        //    _httpClient = httpClient;
+        //}
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
